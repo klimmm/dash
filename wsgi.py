02eb62ecf54892a16f5c01f52c68bd61ce3c1d1d
klimmm <klim@zhgun.com>
2025-01-10 02:47:57 +0300

    Renamed app folder to application and reorganized files

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
 # Import from the specific file
-from dash_app import server
+from app import server
 
 if __name__ == "__main__":
     server.run()
